125fe3fe1025b75265e2a505dfbc01ab91104a5f
CTR281 <chmartin2.81@gmail.com>
2024-02-16 21:44:40 +0100

    :sparkles: add IsPangram

---
+++ Codewars-in-C-sharp.Tests/IsPangram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Codewars_in_C_sharp.IsPangram;
+
+namespace Codewars_in_C_sharp.Tests.IsPangram;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    public void SampleTests()
+    {
+        Assert.AreEqual(true, Kata.IsPangram("The quick brown fox jumps over the lazy dog."));
+        Assert.That(Kata.IsPangram("OK"), Is.EqualTo(false));
+    }
+}
+
---
+++ Codewars-in-C-sharp/6-kyu/IsPangram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_in_C_sharp.IsPangram;
+
+public static class Kata
+{
+    public static bool IsPangram(string str)
+    {
+        return str.
+        Aggregate(
+            new HashSet<char>(),
+            (alphabet, next) =>
+            {
+                if (char.IsLetter(next)) alphabet.Add(char.ToLower(next));
+                return alphabet;
+            },
+            alphabet => alphabet.Count == 26
+        );
+    }
+}
+
