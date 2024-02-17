9124d80ddacb449f41d5cf71f8857cf87c70ae18
CTR281 <chmartin2.81@gmail.com>
2024-02-18 00:02:49 +0100

    :sparkles: add FindEvenIndex

---
+++ Codewars-in-C-sharp.Tests/FindEvenIndex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Codewars_in_C_sharp.FindEvenIndex;
+
+namespace Codewars_in_C_sharp.Tests;
+
+[TestFixture]
+internal class FindEvenIndexTest
+{
+    [Test]
+    public void GenericTests()
+    {
+        Assert.AreEqual(3, Kata.FindEvenIndex(new int[] { 1, 2, 3, 4, 3, 2, 1 }));
+        Assert.AreEqual(1, Kata.FindEvenIndex(new int[] { 1, 100, 50, -51, 1, 1 }));
+        Assert.AreEqual(-1, Kata.FindEvenIndex(new int[] { 1, 2, 3, 4, 5, 6 }));
+        Assert.AreEqual(3, Kata.FindEvenIndex(new int[] { 20, 10, 30, 10, 10, 15, 35 }));
+    }
+}
+
---
+++ Codewars-in-C-sharp/6-kyu/FindEvenIndex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_in_C_sharp.FindEvenIndex;
+
+public class Kata
+{
+    public static int FindEvenIndex(int[] arr)
+    {
+        int n = arr.Length;
+        int[] prefixSumLeft = new int[n];
+        for (int k = 0; k < n - 1; k++)
+        {
+            prefixSumLeft[k + 1] = prefixSumLeft[k] + arr[k];
+        }
+
+        int[] prefixSumRight = new int[n];
+        for (int k = n - 2; k >= 0; k--)
+        {
+            prefixSumRight[k] = prefixSumRight[k + 1] + arr[k + 1];
+        }
+
+        for (int k = 0; k < n; k++)
+        {
+            if (prefixSumLeft[k] == prefixSumRight[k]) return k;
+        }
+        return -1;
+    }
+}
