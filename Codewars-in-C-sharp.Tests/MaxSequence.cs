2edbeba0b7eea9ef93a03703d82645b2e6c49340
CTR281 <chmartin2.81@gmail.com>
2024-02-20 21:07:33 +0100

    :sparkles: add MaxSequence

---
+++ Codewars-in-C-sharp.Tests/MaxSequence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_in_C_sharp.Tests.MaxSequence;
+
+using NUnit.Framework;
+using System;
+using Codewars_in_C_sharp.MaxSequence;
+
+[TestFixture]
+public class SolutionTest
+{
+    [Test]
+    public void Test0()
+    {
+        Assert.AreEqual(0, Kata.MaxSequence(new int[0]));
+    }
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(6, Kata.MaxSequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+    }
+}
---
+++ Codewars-in-C-sharp/5-kyu/MaxSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_in_C_sharp.MaxSequence;
+
+public static class Kata
+{
+    public static int MaxSequence(int[] arr)
+    {
+        if (arr.Length == 0) return 0;
+        int[] dp = new int[arr.Length];
+        dp[0] = arr[0];
+        int result = 0;
+        for (int k = 1; k < arr.Length; k++)
+        {
+            dp[k] = Math.Max(arr[k] + dp[k - 1], arr[k]);
+            result = Math.Max(result, dp[k]);
+        }
+        return result;
+    }
+}
