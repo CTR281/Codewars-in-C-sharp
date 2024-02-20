cf16c558b246fda58051ad9b1dbe909af120aa9c
CTR281 <chmartin2.81@gmail.com>
2024-02-20 20:36:47 +0100

    :sparkles: add Rot13

---
+++ Codewars-in-C-sharp.Tests/Rot13.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_in_C_sharp.Tests.Rot13;
+
+using NUnit.Framework;
+using System;
+using Codewars_in_C_sharp.Rot13;
+
+[TestFixture]
+public class Tests
+{
+    [Test, Description("test")]
+    public void testTest()
+    {
+        Assert.AreEqual("grfg", Kata.Rot13("test"), String.Format("Input: test, Expected Output: grfg, Actual Output: {0}", Kata.Rot13("test")));
+    }
+
+    [Test, Description("Test")]
+    public void TestTest()
+    {
+        Assert.AreEqual("Grfg", Kata.Rot13("Test"), String.Format("Input: Test, Expected Output: Grfg, Actual Output: {0}", Kata.Rot13("Test")));
+    }
+
+    [Test, Description("Test")]
+    public void TestSpecialCharacters()
+    {
+        Assert.AreEqual("10+2 vf gjryir.", Kata.Rot13("10+2 is twelve."), String.Format("Input: 10+2 is twelve., Expected Output: 10+2 vf gjryir., Actual Output: {0}", Kata.Rot13("10+2 is twelve.")));
+        Assert.AreEqual("P# vf pbby!", Kata.Rot13("C# is cool!"), String.Format("Input: C# is cool!, Expected Output: P# vf pbby!, Actual Output: {0}", Kata.Rot13("C# is cool!")));
+    }
+}
---
+++ Codewars-in-C-sharp/6-kyu/Rot13.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_in_C_sharp.Rot13;
+
+public static class Kata
+{
+    public static string Rot13(string message) // 'd' -> (char)('a' + ('d' - 'a' + 13) % 26)
+    {
+        return message
+            .Aggregate(
+            new StringBuilder(), 
+            (sb, next) =>
+                {
+                    if (!char.IsLetter(next)) return sb.Append(next);
+                    if (char.IsLower(next)) return sb.Append((char)('a' + (next - 'a' + 13) % 26));
+                    else return sb.Append((char)('A' + (next - 'A' + 13) % 26));
+                }, 
+            final => final.ToString());
+    }
+}
