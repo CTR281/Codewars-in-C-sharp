2accd473c88b07660e559c57a639639147ae6aa1
CTR281 <chmartin2.81@gmail.com>
2024-02-20 20:14:54 +0100

    :sparkles: add Likes

---
+++ Codewars-in-C-sharp.Tests/Likes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_in_C_sharp.Tests.Likes;
+
+using NUnit.Framework;
+using Codewars_in_C_sharp.Likes;
+
+[TestFixture]
+public class Tests
+{
+    [Test, Description("It should return correct text")]
+    public void SampleTest()
+    {
+        Assert.AreEqual("no one likes this", Kata.Likes(new string[0]));
+        Assert.AreEqual("Peter likes this", Kata.Likes(new string[] { "Peter" }));
+        Assert.AreEqual("Jacob and Alex like this", Kata.Likes(new string[] { "Jacob", "Alex" }));
+        Assert.AreEqual("Max, John and Mark like this", Kata.Likes(new string[] { "Max", "John", "Mark" }));
+        Assert.AreEqual("Alex, Jacob and 2 others like this", Kata.Likes(new string[] { "Alex", "Jacob", "Mark", "Max" }));
+    }
+}
---
+++ Codewars-in-C-sharp/6-kyu/Likes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_in_C_sharp.Likes;
+
+public static class Kata
+{
+    public static string Likes(string[] name)
+    {
+        if (name.Length == 0) return "no one likes this";
+        if (name.Length == 1) return $"{name[0]} likes this";
+        if (name.Length == 2) return $"{name[0]} and {name[1]} like this";
+        if (name.Length == 3) return $"{name[0]}, {name[1]} and {name[2]} like this";
+        return $"{name[0]}, {name[1]} and {name.Length - 2} others like this";
+    }
+}
+
+public static class Kata2
+{
+    public static string Likes(string[] name)
+    {
+        return name.Length switch
+        {
+            0 => "no one likes this",
+            1 => $"{name[0]} likes this",
+            2 => $"{name[0]} and {name[1]} like this",
+            3 => $"{name[0]}, {name[1]} and {name[2]} like this",
+            _ => $"{name[0]}, {name[1]} and {name.Length - 2} others like this"
+        };
+    }
+}
